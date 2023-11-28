6b7fde611d6d8b8d08c3cf90f2727ca37d8b94dc
Nemesix493 <verquerre.kevin@gmail.com>
2023-11-28 12:05:27 +0100

    test : finish test on manage-user endpoint

--- backend/authentication/tests/manage_user.py
+++ backend/authentication/tests/manage_user.py
@@ -6,32 +6,114 @@ UserModel = get_user_model()
 
 
 class TestManageUser(APITestCase):
+    user_data = {
+        'username': 'user_test',
+        'email': 'user_test@exemple.com',
+        'password': 'passwordtest'
+    }
+
+    def setUp(self) -> None:
+        user_test = UserModel.objects.create(
+            **{
+                key: val
+                for key, val in self.user_data.items()
+                if key != 'password'
+            }
+        )
+        user_test.set_password(self.user_data['password'])
+        user_test.save()
+        return super().setUp()
+
     def test_get_user_success(self):
         # Manage user on get by a loged user should return http200 and user data
-        pass
+        user_test = UserModel.objects.get(username=self.user_data['username'])
+        self.client.force_login(user_test)
+        response = self.client.get(reverse_lazy('authentication:manage-user'))
+        self.assertEqual(response.status_code, 200)
 
     def test_get_user_error(self):
         # Manage user on get by a not loged user should return http401
-        pass
+        response = self.client.get(reverse_lazy('authentication:manage-user'))
+        self.assertEqual(response.status_code, 401)
 
     def test_put_user_success(self):
         # Manage user on put by a loged user should return http200 and the updated user data
-        pass
+        user_test = UserModel.objects.get(username=self.user_data['username'])
+        self.client.force_login(user_test)
+        response = self.client.put(
+            path=reverse_lazy('authentication:manage-user'),
+            data={
+                'username': 'new' + user_test.username,
+                'id': user_test.id,
+                'first_name': '',
+                'last_name': ''
+            }
+        )
+        self.assertEqual(response.status_code, 200)
 
     def test_put_user_error(self):
         # Manage user on put by a not loged user should return http401
+        user_test = UserModel.objects.get(username=self.user_data['username'])
+        response = self.client.put(
+            path=reverse_lazy('authentication:manage-user'),
+            data={
+                'username': 'new' + user_test.username,
+                'id': user_test.id,
+                'first_name': '',
+                'last_name': ''
+            }
+        )
+        self.assertEqual(response.status_code, 401)
         # Manage user on put by a loged user with not valid or missing data should return http400
-        pass
+        self.client.force_login(user_test)
+        response = self.client.put(
+            path=reverse_lazy('authentication:manage-user'),
+            data={
+                'username': '',
+                'id': user_test.id,
+                'first_name': '',
+                'last_name': ''
+            }
+        )
+        self.assertEqual(response.status_code, 400)
 
     def test_patch_user_success(self):
         # Manage user on put by a loged user should return http200 and the updated user data
-        pass
+        user_test = UserModel.objects.get(username=self.user_data['username'])
+        self.client.force_login(user_test)
+        response = self.client.put(
+            path=reverse_lazy('authentication:manage-user'),
+            data={
+                'username': 'new' + user_test.username
+            }
+        )
+        self.assertEqual(response.status_code, 200)
 
     def test_patch_user_error(self):
         # Manage user on put by a not loged user should return http401
+        user_test = UserModel.objects.get(username=self.user_data['username'])
+        response = self.client.put(
+            path=reverse_lazy('authentication:manage-user'),
+            data={
+                'username': 'new' + user_test.username
+            }
+        )
+        self.assertEqual(response.status_code, 401)
         # Manage user on put by a loged user with not valid data should return http400
-        pass
+        self.client.force_login(user_test)
+        response = self.client.put(
+            path=reverse_lazy('authentication:manage-user'),
+            data={
+                'username': ''
+            }
+        )
+        self.assertEqual(response.status_code, 400)
 
     def test_other_method_error(self):
         # Any other HTTP verb should return http405
-        pass
+        user_test = UserModel.objects.get(username=self.user_data['username'])
+        self.client.force_login(user_test)
+        response = self.client.delete(
+            path=reverse_lazy('authentication:manage-user')
+        )
+        self.assertEqual(response.status_code, 405)
